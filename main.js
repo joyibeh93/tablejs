30a5dbfc342e31811418f44362f751b37eafb2c7
JOYCE <joychinwenduibeh93@gmail.com>
2022-07-02 13:33:53 +0100

    https://github.com/joyibeh93/tablejs.git

---
+++ main.js
@@ -0,0 +1,99 @@
+// document.write("boy");
+
+let information =[
+    {person1:[1,"Thompson","Anthony",32,"male","Head of Finance department","17/07/2014"]},
+    {person2:[2,"Johnson","Mary",23,"female", "Software Enginneer","05/03/2019"]},
+    {person3:[3,"Chisom","Onyeka",28,"female", "Human Resource","21/06/2016"]},
+    {person4:[4,"Jide","Olabisi",29,"male", "Assistant head(Software department)","05/06/2016"]},
+    {person5:[5,"Williams","Ajayi",30,"male", "Head of Software Engineer","05/09/2015"]},
+    {person6:[6,"Nancy","Chukwuemeka",27,"Female", "Secretary","23/08/2016"]},
+    {person7:[7,"Nicholas","Uchemba",26,"male", "Software Engineer","22/02/2020"]},
+    {person8:[8,"Ahmed","Olayinka",24,"Male", "Graphic designer","15/08/2020"]},
+    {person9:[9,"Halima","mohammed",26,"Female", "Senior Finance Manager","30/05/2018"]},
+    {person10:[10,"Richard","Adeyemi",26,"Male", "CEO","17/11/2010"]}
+
+
+]
+
+    document.getElementById("td1").innerHTML=information[0].person1[0]
+    document.getElementById("td2").innerHTML=information[0].person1[1]
+    document.getElementById("td3").innerHTML=information[0].person1[2]
+    document.getElementById("td4").innerHTML=information[0].person1[3]
+    document.getElementById("td5").innerHTML=information[0].person1[4]
+    document.getElementById("td6").innerHTML=information[0].person1[5]
+    document.getElementById("td7").innerHTML=information[0].person1[6]
+
+
+    document.getElementById("td8").innerHTML=information[1].person2[0]
+    document.getElementById("td9").innerHTML=information[1].person2[1]
+    document.getElementById("td10").innerHTML=information[1].person2[2]
+    document.getElementById("td11").innerHTML=information[1].person2[3]
+    document.getElementById("td12").innerHTML=information[1].person2[4]
+    document.getElementById("td13").innerHTML=information[1].person2[5]
+    document.getElementById("td14").innerHTML=information[1].person2[6]
+
+    document.getElementById("td15").innerHTML=information[2].person3[0]
+    document.getElementById("td16").innerHTML=information[2].person3[1]
+    document.getElementById("td17").innerHTML=information[2].person3[2]
+    document.getElementById("td18").innerHTML=information[2].person3[3]
+    document.getElementById("td19").innerHTML=information[2].person3[4]
+    document.getElementById("td20").innerHTML=information[2].person3[5]
+    document.getElementById("td21").innerHTML=information[2].person3[6]
+
+    document.getElementById("td22").innerHTML=information[3].person4[0]
+    document.getElementById("td23").innerHTML=information[3].person4[1]
+    document.getElementById("td24").innerHTML=information[3].person4[2]
+    document.getElementById("td25").innerHTML=information[3].person4[3]
+    document.getElementById("td26").innerHTML=information[3].person4[4]
+    document.getElementById("td27").innerHTML=information[3].person4[5]
+    document.getElementById("td28").innerHTML=information[3].person4[6]
+
+    document.getElementById("td29").innerHTML=information[4].person5[0]
+    document.getElementById("td30").innerHTML=information[4].person5[1]
+    document.getElementById("td31").innerHTML=information[4].person5[2]
+    document.getElementById("td32").innerHTML=information[4].person5[3]
+    document.getElementById("td33").innerHTML=information[4].person5[4]
+    document.getElementById("td34").innerHTML=information[4].person5[5]
+    document.getElementById("td35").innerHTML=information[4].person5[6]
+
+    document.getElementById("td36").innerHTML=information[5].person6[0]
+    document.getElementById("td37").innerHTML=information[5].person6[1]
+    document.getElementById("td38").innerHTML=information[5].person6[2]
+    document.getElementById("td39").innerHTML=information[5].person6[3]
+    document.getElementById("td40").innerHTML=information[5].person6[4]
+    document.getElementById("td41").innerHTML=information[5].person6[5]
+    document.getElementById("td42").innerHTML=information[5].person6[6]
+
+    document.getElementById("td43").innerHTML=information[6].person7[0]
+    document.getElementById("td44").innerHTML=information[6].person7[1]
+    document.getElementById("td45").innerHTML=information[6].person7[2]
+    document.getElementById("td46").innerHTML=information[6].person7[3]
+    document.getElementById("td47").innerHTML=information[6].person7[4]
+    document.getElementById("td48").innerHTML=information[6].person7[5]
+    document.getElementById("td49").innerHTML=information[6].person7[6]
+
+    document.getElementById("td50").innerHTML=information[7].person8[0]
+    document.getElementById("td51").innerHTML=information[7].person8[1]
+    document.getElementById("td52").innerHTML=information[7].person8[2]
+    document.getElementById("td53").innerHTML=information[7].person8[3]
+    document.getElementById("td54").innerHTML=information[7].person8[4]
+    document.getElementById("td55").innerHTML=information[7].person8[5]
+    document.getElementById("td56").innerHTML=information[7].person8[6]
+
+    document.getElementById("td57").innerHTML=information[8].person9[0]
+    document.getElementById("td58").innerHTML=information[8].person9[1]
+    document.getElementById("td59").innerHTML=information[8].person9[2]
+    document.getElementById("td60").innerHTML=information[8].person9[3]
+    document.getElementById("td61").innerHTML=information[8].person9[4]
+    document.getElementById("td62").innerHTML=information[8].person9[5]
+    document.getElementById("td63").innerHTML=information[8].person9[6]
+
+    document.getElementById("td64").innerHTML=information[9].person10[0]
+    document.getElementById("td65").innerHTML=information[9].person10[1]
+    document.getElementById("td66").innerHTML=information[9].person10[2]
+    document.getElementById("td67").innerHTML=information[9].person10[3]
+    document.getElementById("td68").innerHTML=information[9].person10[4]
+    document.getElementById("td69").innerHTML=information[9].person10[5]
+    document.getElementById("td70").innerHTML=information[9].person10[6]
+   
+   
